07d4e6ffc65c069deba457b5da0272a12cc414e9
smitinit <patelsmit090305@gmail.com>
2026-02-11 12:23:12 +0530

    spell

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -26,7 +26,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'Task Schedular',
+        title: 'Task Scheduler',
       },
     ],
     links: [
